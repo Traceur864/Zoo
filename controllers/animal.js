2189f6422c905bf72dfe7f014ec34ddb09b7ac87
Edwin Carpio <twinsanity864@hotmail.com>
2024-09-22 15:38:28 -0600

    animalss

--- controllers/animal.js
+++ controllers/animal.js
@@ -15,6 +15,96 @@ function proves(req, res){
     });
 }
 
+// Metodo para guardar animales
+async function saveAnimal(req, res) {
+    try {
+        const animal = new Animal();
+        const params = req.body;
+
+        if (params.name) {
+            animal.name = params.name;
+            animal.description = params.description;
+            animal.year = params.year;
+            animal.image = null;
+            animal.user = req.user.sub;
+
+            const animalStored = await animal.save();
+
+            if (!animalStored) {
+                return res.status(404).send({
+                    message: 'No se ha guardado el animal'
+                });
+            } else {
+                return res.status(200).send({
+                    animal: animalStored
+                });
+            }
+        } else {
+            return res.status(400).send({
+                message: 'El nombre del animal es obligatorio'
+            });
+        }
+    } catch (err) {
+        return res.status(500).send({
+            message: 'Error al guardar el animal',
+            error: err.message
+        });
+    }
+}
+
+// Metodo para obtener animales
+
+function getAnimals(req, res) {
+    Animal.find({}).populate({path:'user'}).exec()
+       .then(animals => {
+            if (!animals) {
+                return res.status(404).send({
+                    message: 'No hay animales'
+                });
+            } else {
+                return res.status(200).send({
+                    animals
+                });
+            }
+        })
+       .catch(err => {
+            return res.status(500).send({
+                message: 'Error al obtener los animales',
+                error: err.message
+            });
+        });
+}
+
+// Metodo para obtener animal por usuario
+
+async function getAnimal(req, res) {
+
+    var animalId = req.params.id;
+
+    Animal.findById(animalId).populate({path:'user'}).exec()
+    .then(animal => {
+        if (!animal) {
+            return res.status(404).send({
+                message: 'No hay animal'
+            });
+        } else {
+            return res.status(200).send({
+                animal
+            });
+        }
+    })
+   .catch(err => {
+        return res.status(500).send({
+            message: 'Error al obtener el animal',
+            error: err.message
+        });
+    });
+}
+
+
 module.exports = {
-    proves
+    proves,
+    saveAnimal,
+    getAnimals,
+    getAnimal
 }
--- routes/animal.js
+++ routes/animal.js
@@ -10,6 +10,9 @@ const multipart = require('connect-multiparty');
 const md_upload = multipart({uploadDir: './uploads/animals'});
 
 api.get('/pruebas-animales', md_auth.ensureAuth, AnimalController.proves);
+api.post('/animal', md_auth.ensureAuth, AnimalController.saveAnimal);
+api.get('/animals', AnimalController.getAnimals);
+api.get('/animal/:id', AnimalController.getAnimal);
 
 
 module.exports = api;
