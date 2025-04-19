311b1e69a2c4a751bd5d9f2eaace807991ec0e91
Edwin Carpio <twinsanity864@hotmail.com>
2025-04-19 10:19:26 -0600

    updateAngular

--- controllers/animal.js
+++ controllers/animal.js
@@ -101,10 +101,108 @@ async function getAnimal(req, res) {
     });
 }
 
+// Metodo para actualizar animales
+
+async function updateAnimal(req, res) {
+    var animalId = req.params.id;
+    var update = req.body;
+
+    Animal.findByIdAndUpdate(animalId, update, { new: true })
+       .populate({path:'user'}).exec()
+       .then(animalUpdated => {
+            if (!animalUpdated) {
+                return res.status(404).send({
+                    message: 'No se ha actualizado el animal'
+                });
+            } else {
+                return res.status(200).send({
+                    animal: animalUpdated
+                });
+            }
+        })
+       .catch(err => {
+            return res.status(500).send({
+                message: 'Error al actualizar el animal',
+                error: err.message
+            });
+        });
+}
+
+async function uploadImage(req, res){
+    var animalId = req.params.id;
+    var file_name = 'No subido...';
+
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'png' || file_ext == 'gif') {
+    
+            try {
+                const animalUpdated = await Animal.findByIdAndUpdate(animalId,{image:file_name}, { new: true });
+                if (!animalUpdated) {
+                    return res.status(404).send({ message: 'No se ha podido actualizar el animal' });
+                }
+                return res.status(200).send({ animal: animalUpdated, image: file_name });
+            } catch (err) {
+                return res.status(500).send({ message: 'Error al actualizar animal' });
+            }
+        }else{
+            fs.unlink(file_path, (err) => {
+                if (err){
+                    res.status(500).send({ message: 'Error al eliminar archivo anterior', error: err.message });
+                    console.log('Archivo eliminado');
+                }else{
+                    res.status(400).send({ message: 'Extensión no valida' });
+                }
+            });
+        }
+    }else{
+        res.status(400).send({ message: 'No se han subido archivos' });
+    }
+}
+
+function getImageFile(req, res){
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/animals/' + imageFile;
+
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(404).send({ message: 'La imagen no existe' });
+        }  
+        //Si no existe el archivo, envía un mensaje de error 404.  //Si existe, envía el archivo.  // Si hay algún error, envía un mensaje de error 500.  // Esto es un ejemplo simple y puede cambiar según sea necesario.  // En el ejemplo anterior, la imagen se encuentra en la carpeta 'uploads/users/'.  // Si la carpeta 'uploads' no existe, se creará automáticamente.  // La carpeta 'users' también se creará automáticamente si no existe.  // El nombre de la imagen se envía como parámetro en la URL.
+    })
+}
+
+function deleteAnimal(req, res){
+    var animalId = req.params.id;
+
+    Animal.findByIdAndDelete(animalId)
+       .then(animalDeleted => {
+            if (!animalDeleted) {
+                return res.status(404).send({ message: 'No se ha podido eliminar el animal' });
+            } else {
+                return res.status(200).send({ message: 'Animal eliminado' });
+            }
+        })
+       .catch(err => {
+            return res.status(500).send({ message: 'Error al eliminar el animal', error: err.message });
+        });
+}
 
 module.exports = {
     proves,
     saveAnimal,
     getAnimals,
-    getAnimal
+    getAnimal,
+    updateAnimal,
+    uploadImage,
+    getImageFile,
+    deleteAnimal
 }
--- routes/animal.js
+++ routes/animal.js
@@ -5,14 +5,18 @@ const AnimalController = require('../controllers/animal');
 
 const api = express.Router();
 const md_auth = require('../middlewares/authenticated');
+const md_admin = require('../middlewares/is_admin');
 
 const multipart = require('connect-multiparty');
 const md_upload = multipart({uploadDir: './uploads/animals'});
 
 api.get('/pruebas-animales', md_auth.ensureAuth, AnimalController.proves);
-api.post('/animal', md_auth.ensureAuth, AnimalController.saveAnimal);
+api.post('/animal', [md_auth.ensureAuth, md_admin.isAdmin], AnimalController.saveAnimal);
 api.get('/animals', AnimalController.getAnimals);
 api.get('/animal/:id', AnimalController.getAnimal);
-
+api.put('/animal/:id', [md_auth.ensureAuth, md_admin.isAdmin], AnimalController.updateAnimal);
+api.post('/upload-image-animal/:id', [md_auth.ensureAuth, md_admin.isAdmin, md_upload], AnimalController.uploadImage);
+api.get('/get-image-animal/:imageFile', AnimalController.getImageFile);
+api.delete('/animal/:id', [md_auth.ensureAuth, md_admin.isAdmin, md_upload], AnimalController.deleteAnimal);
 
 module.exports = api;
